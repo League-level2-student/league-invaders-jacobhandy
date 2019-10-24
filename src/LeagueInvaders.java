2906e5f01e58b2315574edfb4f99a3969af69f2f
jacobhandy <jacobhandy@192.168.1.8>
2019-10-23 21:10:25 -0700

    League Invaders 10/23/19

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,23 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	public static int frameWidth = 500;
+	public static int frameHeight = 800;
+	public static JFrame mainFrame;
+	public LeagueInvaders() {
+		mainFrame = new JFrame();
+	}
+  public static void createWindow() {
+    	    Dimension d = new Dimension(frameWidth, frameHeight);
+		
+		mainFrame.setSize(d);
+		mainFrame.setTitle("League Invaders! (Space Invaders ripped off of us!)");
+}
+  public static  void setup() {
+	  createWindow();
+	  mainFrame.setVisible(true);
+	  mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 }
---
+++ src/LeagueInvadersRunner.java
@@ -0,0 +1,7 @@
+
+public class LeagueInvadersRunner {
+ public static void main(String[] args) {
+	LeagueInvaders game = new LeagueInvaders();
+	game.setup();
+}
+}
